f9f155d8d865239c6ae121ca089f5d84020f448c
egodschalk <egodschalk@gmail.com>
2024-08-20 23:32:58 -0400

    user model 1st pass

--- models/User.js
+++ models/User.js
@@ -1,43 +1,60 @@
 const { Schema, model } = require('mongoose');
 
-// // Schema to create User model
-// const userSchema = new Schema(
-//   {
-//     first: String,
-//     last: String,
-//     age: Number,
-//     videos: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'video',
-//       },
-//     ],
-//   },
-//   {
-//     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-//     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-//     toJSON: {
-//       virtuals: true,
-//     },
-//     id: false,
-//   }
-// );
+var validateEmail = function (email) {
+    var re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return re.test(email)
+};
 
-// // Create a virtual property `fullName` that gets and sets the user's full name
-// userSchema
-//   .virtual('fullName')
-//   // Getter
-//   .get(function () {
-//     return `${this.first} ${this.last}`;
-//   })
-//   // Setter to set the first and last name
-//   .set(function (v) {
-//     const first = v.split(' ')[0];
-//     const last = v.split(' ')[1];
-//     this.set({ first, last });
-//   });
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+    },
+    thoughts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+    },
+    ],
+    friends: [
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    ]
+    {
+        // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+        // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
-// // Initialize our User model
-// const User = model('user', userSchema);
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema
+    .virtual('friendCount')
+    // Getter
+    .get(function () {
+        return this.friends.length;
+    })
+    // Setter to set the first and last name
+    .set(function (v) {
+        // Do I need a .set?
+    });
+
+// Initialize our User model
+const User = model('user', userSchema);
 
 module.exports = User;
